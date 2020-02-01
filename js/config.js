02904128ec323ca07b3e8599cf02e5b82b4d05ce
Joe8bgh <53146900+Joe8bgh@users.noreply.github.com>
2020-01-31 20:23:35 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_rqkmwwvtt', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'b8h4qua80p7rih20470qv5hn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '11uma9ffqt88b2r4ul63be3hl2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://hvagmpkvf7.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://2fpzbcpxhj.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
